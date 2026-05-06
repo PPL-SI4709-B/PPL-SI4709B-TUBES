424923195469c31263f45147813dd4f090cddcbb
Davi-ARD <davi.ardian17@gmail.com>
2026-05-06 16:41:03 +0700

    Testing PBI-13 Pengajuan Program Pembinaan - Mengajukan program pendampingan akses layanan pembiayaan

---
+++ tests/Browser/PBI13PengajuanTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+
+class PBI13PengajuanTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Test Positive: Berhasil melakukan pengajuan program
+     */
+    public function test_positive_pengajuan_berhasil()
+    {
+        $user = User::factory()->create([
+            'role' => 'umkm'
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                    ->visit('/umkm/pengajuan')
+                    ->press('Ajukan Program')
+                    ->waitFor('#modal-pengajuan')
+                    ->type('kebutuhan_usaha', 'Butuh pinjaman untuk ekspansi')
+                    ->press('Kirim Pengajuan')
+                    ->waitForText('Berhasil!', 5)
+                    ->assertSee('Berhasil!');
+        });
+    }
+
+    /**
+     * Test Negative: Gagal melakukan pengajuan karena data tidak lengkap
+     */
+    public function test_negative_pengajuan_tanpa_kebutuhan()
+    {
+        $user = User::factory()->create([
+            'role' => 'umkm'
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                    ->visit('/umkm/pengajuan')
+                    ->press('Ajukan Program')
+                    ->waitFor('#modal-pengajuan')
+                    ->script("document.getElementById('kebutuhan_usaha').removeAttribute('required');");
+            
+            $browser->press('Kirim Pengajuan')
+                    ->pause(1000)
+                    ->assertDontSee('Berhasil!')
+                    ->assertPathIs('/umkm/pengajuan');
+        });
+    }
+}
